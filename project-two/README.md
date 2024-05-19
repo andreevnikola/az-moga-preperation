09b1a377cbc41e9a4f2d9f5c0cad6d3d00d98b28
Vladimir Pasev <vladigpasev@gmail.com>
2024-05-19 23:46:14 +0300

    Config project two

---
+++ project-two/README.md
@@ -0,0 +1,3 @@
+# This is the second project in which we will be preparing for the contest
+
+# In the contest itself we can be using this as a starting point
---
+++ project-two/src/index.ts
@@ -0,0 +1,7 @@
+// Import any necessary modules or libraries here
+
+// Define your TypeScript code here
+
+// Example code
+const greeting: string = "Hello, world!";
+console.log(greeting);
